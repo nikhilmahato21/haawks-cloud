80f57352186fc25f4e104dae56a7b7d8ab35d120
nikhil <nikhilmahato3337@gmail.com>
2024-11-21 21:33:47 +0530

    colors issue

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -21,13 +21,13 @@ const About = () => {
           />
         </div>
         <div className="max-w-3xl  flex flex-col justify-center items-center  ">
-          <h2 className="font-bold mt-8 lg:mt-0 text-black mb-4 md:mb-8 text-5xl bg-clip-text  text-center  px-6 drop-shadow-2xl  bg-gradient-to-b from-white/80 to-white/90 max-w-2xl">
+          <h2 className="font-bold mt-8 lg:mt-0 text-black mb-4 md:mb-8 text-5xl bg-clip-text  text-center  px-6  max-w-2xl">
             About Our{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-[#4d229e]">
               Company
             </span>
           </h2>
-          <p className="bg-clip-text text-black  text-center font-bold px-2  drop-shadow-2xl text-xl  max-w-2xl">
+          <p className=" text-black  text-center font-bold px-2 text-xl  max-w-2xl">
             At Haawks, we are revolutionizing the future of data storage. Our
             decentralized cloud platform ensures your data is not just
             stored—but safeguarded, secured, and controlled by you. With
